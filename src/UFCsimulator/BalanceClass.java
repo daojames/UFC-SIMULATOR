dc136f1fb970eaac7d38c7e0a90f7b70e225436a
kentedstrom <43936496+kentedstrom@users.noreply.github.com>
2023-06-05 19:36:52 -0700

    Update BalanceClass.java
    Change public variables to private and refactor calcClout

--- src/UFCsimulator/BalanceClass.java
+++ src/UFCsimulator/BalanceClass.java
@@ -2,10 +2,10 @@ package UFCsimulator;
 
 public class BalanceClass {
 
-	int balance = 50000;
-	int twitter = 10000;
-	int clout;
-	int sponsor = 0;
+	private int balance = 50000;
+	private int twitter = 10000;
+	private int clout;
+	private int sponsor = 0;
 	
 	public BalanceClass() {}
 	
@@ -34,46 +34,49 @@ public class BalanceClass {
 	}
 	
 	public void calcClout() {
-		int twitter = this.getTwitter();
-		int pos = Weight.Player.getPos();
-		double pos1 = 1;
+	    int twitter = this.getTwitter();
+	    int pos = Weight.Player.getPos();
+	    double pos1 = 1;
 
-		if (pos == 10) {
-			pos1 = 1.0;
-		}
-		else if (pos == 9) {
-			pos1 = 1.1;
-		}
-		else if (pos == 8) {
-			pos1 = 1.2;
-		}
-		else if (pos == 7) {
-			pos1 = 1.3;
-		}
-		else if (pos == 6) {
-			pos1 = 1.4;
-		}
-		else if (pos == 5) {
-			pos1 = 1.5;
-		}
-		else if (pos == 4) {
-			pos1 = 1.6;
-		}
-		else if (pos == 3) {
-			pos1 = 1.7;
-		}
-		else if (pos == 2) {
-			pos1 = 1.8;
-		}
-		else if (pos == 1) {
-			pos1 = 1.9;
-		}
-		else if (pos == 0) {
-			pos1 = 3.0;
-		}
-		
-		this.clout = (int) (twitter * pos1);
-	}
+	    switch (pos) {
+		case 10:
+		    pos1 = 1.0;
+		    break;
+		case 9:
+		    pos1 = 1.1;
+		    break;
+		case 8:
+		    pos1 = 1.2;
+		    break;
+		case 7:
+		    pos1 = 1.3;
+		    break;
+		case 6:
+		    pos1 = 1.4;
+		    break;
+		case 5:
+		    pos1 = 1.5;
+		    break;
+		case 4:
+		    pos1 = 1.6;
+		    break;
+		case 3:
+		    pos1 = 1.7;
+		    break;
+		case 2:
+		    pos1 = 1.8;
+		    break;
+		case 1:
+		    pos1 = 1.9;
+		    break;
+		case 0:
+		    pos1 = 3.0;
+		    break;
+	    }
+    
+    this.clout = (int) (twitter * pos1);
+}
+
 	
 	public int getClout() {
 		return this.clout;
